731e5ae979880aa9d47c3f4ade292a290f4d8a69
gairin <marco14963@gmail.com>
2023-04-27 01:00:02 -0300

    Update ex06.cpp

--- Lista01/ex06.cpp
+++ Lista01/ex06.cpp
@@ -86,7 +86,7 @@ int main()
 
 	// Gerando um buffer simples, com a geometria de um triângulo
 	//GLuint VAO = setupGeometry();
-	int nPoints = 20;
+	int nPoints = 200;
 	GLuint VAO = createCircle(0.5, nPoints);
 
 	glUseProgram(shader.ID);
